00fdac52bc0870bb051b0c4497f313a93ac493c2
Kenneth Ekandem <kennyekandem@gmail.com>
2021-04-14 16:17:29 +0100

    update

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
 const express = require("express");
 const path = require("path");
 const members = require("./members");
-const moment = require("moment");
+const logger = require("./middelware/logger");
+
 
 const app = express();
 
-const logger = (req, res, next) => {
-  console.log(
-    `${req.protocol}://${req.get("host")}${
-      req.originalUrl
-    } : ${moment().format()}`
-  );
-  next();
-};
-
 // init middleware
-app.use(logger);
+// app.use(logger);
 
 // Gets all members
 app.get("/api/members", (req, res) => res.json(members));
+
+// get single member
+app.get("/api/members/:id", (req, res) => {
+  res.json(members.filter(member => member.id === parseInt(req.params.id)));
+})
 
 /*app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
