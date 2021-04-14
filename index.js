d08ca40e0bfceb5dd20479ee5e5de03df839ea3f
Kenneth Ekandem <kennyekandem@gmail.com>
2021-04-14 16:33:50 +0100

    update

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-const members = require("./members");
 const logger = require("./middelware/logger");
 
 
@@ -9,27 +8,13 @@ const app = express();
 // init middleware
 // app.use(logger);
 
-// Gets all members
-app.get("/api/members", (req, res) => res.json(members));
-
-// get single member
-app.get("/api/members/:id", (req, res) => {
-
-  const found = members.some(member => member.id === parseInt(req.params.id));
-
-  if(found) {
-    res.json(members.filter(member => member.id === parseInt(req.params.id)));
-  }else {
-    res.status(400).json({ msg: `No member with the id of ${req.params.id}`});
-  }
-
-})
-
 /*app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })*/
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use('/api/members', require('./routes/api/members'));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`server started on port ${port}`));
