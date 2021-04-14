83fd2cb140864774af0949453e9a7a0e55fd9740
Kenneth Ekandem <kennyekandem@gmail.com>
2021-04-14 16:02:22 +0100

    update

--- index.js
+++ index.js
@@ -1,39 +1,30 @@
-const express = require('express');
+const express = require("express");
 const path = require("path");
-
+const members = require("./members");
+const moment = require("moment");
 
 const app = express();
 
-const members = [
-    {
-        id: 1,
-        name: 'John Doe',
-        email: 'johndoe@mail.com',
-        status: 'active'
-    },
-    {
-        id: 2,
-        name: 'Jane Doe',
-        email: 'janedoe@mail.com',
-        status: 'in-active'
-    },
-    {
-        id: 3,
-        name: 'Jackson Doe',
-        email: 'jacksondoe@mail.com',
-        status: 'active'
-    },
+const logger = (req, res, next) => {
+  console.log(
+    `${req.protocol}://${req.get("host")}${
+      req.originalUrl
+    } : ${moment().format()}`
+  );
+  next();
+};
 
-]
+// init middleware
+app.use(logger);
 
 // Gets all members
-app.get('/api/members', (req, res) => res.json(members));
+app.get("/api/members", (req, res) => res.json(members));
 
 /*app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })*/
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`server started on port ${port}`));
---
+++ members.js
@@ -0,0 +1,23 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'johndoe@mail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Jane Doe',
+        email: 'janedoe@mail.com',
+        status: 'in-active'
+    },
+    {
+        id: 3,
+        name: 'Jackson Doe',
+        email: 'jacksondoe@mail.com',
+        status: 'active'
+    },
+
+]
+
+module.exports = members;
