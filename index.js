bcf3460b37b9c5c836bef2d969dd1745c07d0162
Kenneth Ekandem <kennyekandem@gmail.com>
2021-04-15 09:01:42 +0100

    update

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const path = require("path");
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars");
 const logger = require("./middelware/logger");
-
+const members = require("./members");
 
 const app = express();
 
 // body parser middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
+
+// Home page route
+app.get("/", (req, res) => {
+  res.render("index", {
+      title: "Member App",
+      members
+  });
+});
 
 // handle bars middleware
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // init middleware
 // app.use(logger);
@@ -24,7 +32,7 @@ app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, "public")));
 
 // members API routes
-app.use('/api/members', require('./routes/api/members'));
+app.use("/api/members", require("./routes/api/members"));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`server started on port ${port}`));
